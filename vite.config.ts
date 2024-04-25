8a14ae081c78da01c89000a9ae1ed8787d94d437
nyaomaru <nyaonyao0725@gmail.com>
2024-04-25 20:04:00 +0900

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    watch: {
+      usePolling: true
+    }
+  },
+  plugins: [react()],
+})
