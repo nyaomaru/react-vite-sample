2d8bc0a12236716d62e59c8f8a422ac208fad1c7
nyaomaru <nyaonyao0725@gmail.com>
2024-05-11 19:34:59 +0900

    Create customer detail api

---
+++ src/features/customer/Customer.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const CardContentStyle = style({
+  textAlign: 'left',
+});
+
+export const ButtonStyle = style({
+  margin: '1rem',
+});
--- src/features/customer/CustomerDetail.tsx
+++ src/features/customer/CustomerDetail.tsx
@@ -1,8 +1,15 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
+import { ErrorAlert } from '@/components/atoms/ErrorAlert';
 import { SimpleButton } from '@/components/atoms/SimpleButton';
 import { useAuthCheck } from '@/hooks/useAuthCheck';
+import { CustomerDetailQueries } from '@/queries/customer.queries';
 import { PATH } from '@/pages/router/const';
+
+import { ButtonStyle } from './Customer.css';
+
+import { DetailCard } from './DetailCard';
 
 export const CustomerDetail = () => {
   useAuthCheck();
@@ -10,16 +17,25 @@ export const CustomerDetail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
+  const { data, isError, error } = useQuery({
+    ...CustomerDetailQueries.getCustomerDetail(Number(id)),
+  });
+
   return (
     <>
       <h1>Customer detail page</h1>
       <h2>ID: {id}</h2>
-      <SimpleButton
-        buttonName="Return"
-        buttonType="button"
-        color="secondary"
-        onClick={() => navigate(PATH.CUSTOMER)}
-      ></SimpleButton>
+      {isError && <ErrorAlert errorMessage={error.message} />}
+      {data !== undefined && <DetailCard data={data} />}
+
+      <div className={ButtonStyle}>
+        <SimpleButton
+          buttonName="Return"
+          buttonType="button"
+          color="secondary"
+          onClick={() => navigate(PATH.CUSTOMER)}
+        ></SimpleButton>
+      </div>
     </>
   );
 };
