f0abe546347f74bf4de588bd774e0271097637c7
nyaomaru <nyaonyao0725@gmail.com>
2024-05-18 12:28:40 +0900

    Set snapshot test config

---
+++ snapshot-resolver.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+export default {
+  resolveSnapshotPath: (testPath) => {
+    const fileName = path.basename(testPath);
+    const fileNameWithoutExtension = fileName.replace(/\.[^/.]+$/, '');
+    const modifiedFileName = `${fileNameWithoutExtension}.snap`;
+
+    return path.join('./__snapshots__', modifiedFileName);
+  },
+  resolveTestPath: (snapshotFilePath, snapshotExtension) =>
+    path.basename(snapshotFilePath, snapshotExtension),
+  testPathForConsistencyCheck: 'example',
+};
