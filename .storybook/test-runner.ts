9ae5eb428b7867924538f7dbff0c6c76a68853fe
nyaomaru <nyaonyao0725@gmail.com>
2024-06-15 14:26:39 +0900

    refactor: settings

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,9 +1,6 @@
-/// <reference types="jest" />
-
 import type { TestRunnerConfig } from '@storybook/test-runner';
 
 const config: TestRunnerConfig = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async postVisit(page, _context) {
     const elementHandler = await page.$('#storybook-root');
     const innerHTML = await elementHandler?.innerHTML();
